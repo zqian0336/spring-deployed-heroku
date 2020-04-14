a045a44b5d0c4a04201c17ee0060bfefe162c7a3
Spencer <zqian0336@gmail.com>
2020-04-14 16:16:10 -0700

    deployed

---
+++ src/main/resources/static/precache-manifest.801419a07556d9127cc4748fc5cd859f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943d6f7aca7210c218fdcdbb9d66bff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257f71bd32de333f7938",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "41cab12b7071285670d3",
+    "url": "/static/css/main.00b7f348.chunk.css"
+  },
+  {
+    "revision": "257f71bd32de333f7938",
+    "url": "/static/js/2.77a8222e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.77a8222e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cab12b7071285670d3",
+    "url": "/static/js/main.fb4afde0.chunk.js"
+  },
+  {
+    "revision": "c5e1e0a252f02dd2aab9",
+    "url": "/static/js/runtime-main.59bbfdb2.js"
+  },
+  {
+    "revision": "f3c56e8440bbc37657940a98f15d5c99",
+    "url": "/static/media/manager.f3c56e84.svg"
+  },
+  {
+    "revision": "6f50130968f84768e88d54d696185f8a",
+    "url": "/static/media/orange.6f501309.mp4"
+  }
+]);
